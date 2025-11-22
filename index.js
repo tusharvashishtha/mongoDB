e0e8baa2330f912384b9ded95df466cb00906c72
tushar vashishtha <vashishthatushar004@gmail.com>
2025-11-22 13:25:29 +0530

    find User

--- index.js
+++ index.js
@@ -2,3 +2,28 @@ const express = require('express');
 const app = express();
 
 require('./main')();
+
+const User = require('./User');
+const { default: mongoose } = require('mongoose');
+
+async function test() {
+    try{
+        const user1 = await User.create({
+            name : "Kim",
+            age : 20
+        })
+        console.log(user1)
+    }
+    catch{
+        console.log(err)
+    }
+}
+
+// test();
+
+async function findUser() {
+    const searchUser = await User.findById('692167f800bb390ebefc0475')
+    console.log(searchUser)
+}
+
+findUser();
